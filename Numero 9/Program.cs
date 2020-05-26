c91961f7eef269065bc656e03fbde0ab5f3eebac
Ariel Diaz Martinez <20199079@itla.edu.do>
2020-05-25 21:18:18 -0400

    Ejercicios commit

---
+++ Numero 9/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un programa que pida al usuario tres números reales y muestre cuál es el
+               mayor de los tres. */
+
+            double num1;
+            double num2;
+            double num3;
+
+            Console.WriteLine("Ingrese el primer numero");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el Segundo numero");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el Tercer numero");
+            num3 = Convert.ToDouble(Console.ReadLine());
+
+             if (num1 >= num2 && num1 >= num3)
+                {
+                    Console.WriteLine("El primer numero es el mayor");
+
+                }
+
+            else
+            {
+                if (num2 >= num3 && num2 >= num1)
+                {
+                    Console.WriteLine("El Segundo numero es el mayor");
+
+                }
+                else
+                {
+                     
+                    if (num3 >= num1 && num3 >= num2)
+                            {
+                                Console.WriteLine("El Tercer numero es el mayor");
+                            }
+                }
+                    
+                      
+
+            }
+            Console.ReadKey();
+            
+        }       
+    }
+}
---
+++ Numero8/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un programa que pida al usuario dos números enteros y diga  "Uno de los
+             números es positivo ", "Los dos números son positivos " o bien "Ninguno de los
+                  números es positivo", según corresponda. */
+
+            int num1;
+            int num2;
+            
+
+            Console.WriteLine("Ingrese el primer numero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el Segundo numero");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 <= 1 && num2 <= 1)
+            {
+                Console.WriteLine("Ninguno de los numeros es positivo");
+            }
+            else
+            {
+                if (num1 <= 1 || num2 <= 1)
+                {
+                    Console.WriteLine("Uno de los números es positivo");
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Los dos números son positivos");
+                }
+            }
+
+           
+
+       
+            
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
